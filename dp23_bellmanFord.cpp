1266b72b2dde7e087ae50b36437ef49807dc5b9d
Sakshi <saksgoya@adobe.com>
2020-12-12 18:11:26 +0530

    Correction in comments of Bellman Ford Algorithm

--- dp23_bellmanFord.cpp
+++ dp23_bellmanFord.cpp
@@ -1,6 +1,6 @@
 int bellmanFord(vector<vector<int>> &graph, int V, int E, int src)
 {
-	//graph[i] represents ith edge from u to v with weight w (u, v, w)
+	//graph[i] represents ith edge (from u to v) with weight w (u, v, w)
 	//check negative weight cycle
 	int dp[V]; //dp[i] contains distance of every vertex from source
 	for(int i=0;i<V;i++)
@@ -20,5 +20,5 @@ int bellmanFord(vector<vector<int>> &graph, int V, int E, int src)
 		 if(dp[graph[j][0]]!=INT_MAX && dp[graph[j][1]] > dp[graph[j][0]] + graph[j][2])
 		 	return 1;
 	}
-	return 0; //print 0 if there is no weight cycle 
+	return 0; //return 0 if there is no weight cycle 
 }
