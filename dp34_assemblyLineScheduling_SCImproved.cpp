8062cfb714fdeed72de26861018caa5a20478f22
Sakshi <saksgoya@adobe.com>
2020-12-19 19:04:41 +0530

    DP-34 Comments Corrected

--- dp34_assemblyLineScheduling_SCImproved.cpp
+++ dp34_assemblyLineScheduling_SCImproved.cpp
@@ -1,7 +1,7 @@
 int assemblyLineScheduling(vector<vector<int>> &a, vector<vector<int>> &t, vector<int> &e, vector<int> &x)
 {
 	//a[i][j] is the time taken at station S[i][j]
-	//t[i][j] is the time t shift from line i at station j-1 to station j
+	//t[i][j] is the time to shift from line i at station j-1 to station j
 	//e[0] denotes the entry time in line 0. Similarly, e[1]
 	//x[0] denotes the exit time from line 0. Similarly, x[1]
 	int n = a[0].size();
