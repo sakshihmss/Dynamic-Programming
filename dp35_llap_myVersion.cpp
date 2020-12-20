7aae5678d80a5b689e72088156c237e735826183
Sakshi <saksgoya@adobe.com>
2020-12-20 11:53:29 +0530

    DP-35 Longest Arithmetic Progression (Subset) added

--- dp35_llap_myVersion.cpp
+++ dp35_llap_myVersion.cpp
@@ -4,10 +4,15 @@ int llap(vector<int> &set)
 	int n = set.size();
 	if(n<=2) return n;
 	int dp[n][n];
-	int ret = 2;
 	for(int i=0;i<n;i++)
-		dp[i][n-1] = 2;
-
+	{
+	    for(int j=0;j<n;j++)
+	    {
+	    	if(i == j) dp[i][j] = 1;
+	        else dp[i][j] = 2;
+	    }
+	}
+	int ret = 2;
 	for(int j=n-2;j>=1;j--)
 	{
 		int i = j-1;
@@ -17,10 +22,7 @@ int llap(vector<int> &set)
 			if(set[i] + set[k] < 2*set[j])
 				k++;
 			else if(set[i] + set[k] > 2*set[j])
-			{
-				dp[i][j] = 2;
 				i--;
-			}
 			else
 			{
 				dp[i][j] = dp[j][k] + 1;
@@ -28,13 +30,6 @@ int llap(vector<int> &set)
 				i--, k++;
 			}	
 		}
-
-		while(i>=0)
-		{
-			dp[i][j] = 2;
-			i--;
-		}
 	}
 	return ret;
 }
-
